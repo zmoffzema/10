478692368b606968a20a0af52bdc9d695cc8d89c
z_moff <zmoffzema@gmail.com>
2024-06-20 00:01:35 +0300

    Добавьте файлы проекта.

---
+++ 10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[,] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int sum = CalculateSum(array);
+        Console.WriteLine("The sum of all elements in the array is: " + sum);
+    }
+
+    public static int CalculateSum(int[,] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                sum += array[i, j];
+            }
+        }
+        return sum;
+    }
+}
